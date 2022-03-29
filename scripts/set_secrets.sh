6ac4d734ea9b84f64f5cb2adbe9b114833341573
Phu Le <phu.le.dev@gmail.com>
2022-03-29 13:03:04 +0000

    update secret name

--- scripts/set_secrets.sh
+++ scripts/set_secrets.sh
@@ -10,7 +10,7 @@ if [ -f ".env" ]; then
     export $(echo $(cat .env | sed 's/#.*//g'| xargs) | envsubst)
     gh secret set AWS_ACCESS_KEY_ID --body "$AWS_ACCESS_KEY_ID"
     gh secret set AWS_SECRET_ACCESS_KEY --body "$AWS_SECRET_ACCESS_KEY"
-    gh secret set AWS_DEFAULT_REGION --body "$AWS_DEFAULT_REGION"
+    gh secret set AWS_REGION --body "$AWS_REGION"
 else
     echo "Please create .env file!"
 fi
